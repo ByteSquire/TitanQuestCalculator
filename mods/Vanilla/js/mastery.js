49c49c94df1da8da3b63f5a88c64e8d5d9dab45b
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2020-12-13 17:59:54 +0100

    [website-fancy] implement mastery bar

--- mods/Vanilla/js/mastery.js
+++ mods/Vanilla/js/mastery.js
@@ -16,6 +16,8 @@ var mod;
 var pointsSpent = 0;
 var m1LevelReq;
 var m2LevelReq;
+var m1CurrTier = 0;
+var m2CurrTier = 0;
 
 var xmlhttp = new XMLHttpRequest();
 var url =
@@ -109,15 +111,39 @@ function plusClicked(button, left){
     var curr = Number(splits[0]);
     var max = Number(splits[1]);
     var updated = curr;
+    var decrease = false;
+    var matteringMastery = (button.parentElement.id.split("panel")[1] == "1")? m1 : m2;
     if(left){
         if (curr < max){
             updated = curr + 1;
             pointsSpent++;
+            mod.masteryLevels.forEach((element) => {
+                if(element == updated){
+                    button.parentElement.getElementsByClassName("bar")[0].style.height = (mod.masteryLevels.indexOf(element)+1) * (80/mod.masteryLevels.length) + "%";
+                    if(matteringMastery == m1)
+                        m1CurrTier++;
+                    else
+                        m2CurrTier++;
+                }
+            });
         }
     } else {
         if (curr > 0){
             updated = curr - 1;
             pointsSpent--;
+            if(m1CurrTier != 0 || m2CurrTier != 0){
+                var matters = mod.masteryLevels[Number((matteringMastery == m1)? m1CurrTier-1 : m2CurrTier-1)]-1;
+                if(updated == matters){
+                    var currHeight = Number(button.parentElement.getElementsByClassName("bar")[0].style.height.split("%")[0]);
+                    button.parentElement.getElementsByClassName("bar")[0].style.height = (currHeight - (80/mod.masteryLevels.length)) + "%";
+                    if(button.parentElement.id.split("panel")[1] == 1)
+                        m1CurrTier--;
+                    else
+                        m2CurrTier--;
+                }
+            }
+            if(updated == 0)
+                button.parentElement.getElementsByClassName("bar")[0].style.height = "0%";
         }
     }
     button.innerHTML = button.innerHTML.replace(curr, updated);
