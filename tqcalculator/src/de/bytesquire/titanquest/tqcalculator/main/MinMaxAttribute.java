e290683d06f0fc55905a9560b1e2de50b5eaf0a8
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2020-12-19 13:00:06 +0100

    [Model] add type for min ~ max values

---
+++ tqcalculator/src/de/bytesquire/titanquest/tqcalculator/main/MinMaxAttribute.java
@@ -0,0 +1,36 @@
+package de.bytesquire.titanquest.tqcalculator.main;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class MinMaxAttribute {
+    private Object mMin;
+    private Object mMax;
+
+    public Object getMin() {
+        return mMin;
+    }
+
+    public void setMin(Object min) {
+        this.mMin = min;
+    }
+
+    public Object getMax() {
+        return mMax;
+    }
+
+    public void setMax(Object max) {
+        this.mMax = max;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder ret = new StringBuilder();
+        ret.append("min: " + mMin.toString());
+        if (mMax != null) {
+            ret.append("\nmax: " + mMax.toString());
+        }
+        return ret.toString();
+    }
+}
