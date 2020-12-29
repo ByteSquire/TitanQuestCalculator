21968fa3e446516d5ec2310cc4969185ef56717c
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2020-12-29 09:17:07 +0100

    [website-fancy] fix missing values for duration attributes

--- mods/SoulvizierAERA/js/attribute_parser.js
+++ mods/SoulvizierAERA/js/attribute_parser.js
@@ -20,9 +20,9 @@ function getAttributeString(key, value, index){
             else
                 return formatAttribute(ret, value.min, index);
         } else if(value.max)
-                return formatAttribute(ret, value.max, index);
-        else if(value.value0)
-            formatAttributeWithSecondValue(value, index);
+            return formatAttribute(ret, value.max, index);
+        else if(value.value0 || value.value1)
+            return formatAttributeWithSecondValue(value, index);
     }
     
     return formatAttribute(ret, value, index);
@@ -30,8 +30,10 @@ function getAttributeString(key, value, index){
 
 function formatAttributeWithSecondValue(value, index){
     var ret = value.key;
-    ret = getAttributeString(ret.replace("value0", "value"), value.value0, index);
-    ret = getAttributeString(ret.replace("value1", "value"), value.value1, index); 
+    if(value.value0)
+        ret = getAttributeString(ret.replace("value0", "value"), value.value0, index);
+    if(value.value1)
+        ret = getAttributeString(ret.replace("value1", "value"), value.value1, index); 
     return ret;
 }
 
