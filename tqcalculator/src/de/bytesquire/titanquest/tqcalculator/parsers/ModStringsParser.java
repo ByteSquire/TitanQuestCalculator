fd9db57bb6105e26c2067ffd437843d2b1382914
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-03-19 06:19:57 +0100

    fix(attributes): add % energy reserved, reverse to null tags

--- tqcalculator/src/de/bytesquire/titanquest/tqcalculator/parsers/ModStringsParser.java
+++ tqcalculator/src/de/bytesquire/titanquest/tqcalculator/parsers/ModStringsParser.java
@@ -53,15 +53,14 @@ public class ModStringsParser {
                             BufferedReader skillReader = new BufferedReader(isr);
                             Stream<String> fileStream = skillReader.lines();
                             fileStream.forEach((str) -> {
-                                if (!str.isBlank())
-                                    if (str.split("=").length > 1) {
-                                        String key = str.split("=")[0];
-                                        String value = str.split("=")[1];
-                                        if (value.split("//").length > 0)
-                                            _tags.put(key, value.split("//")[0]);
-                                        else
-                                            _tags.put(key, value);
-                                    }
+                                if (!str.isBlank() && str.split("=").length > 1) {
+                                    String key = str.split("=")[0];
+                                    String value = str.split("=")[1];
+                                    if (value.split("//").length > 0)
+                                        _tags.put(key, value.split("//")[0]);
+                                    else
+                                        _tags.put(key, value);
+                                }
                             });
                         } catch (Exception e) {
                             Util.logError(LOGGER, e);
@@ -93,7 +92,7 @@ public class ModStringsParser {
                 Util.logError(LOGGER, "tag: " + element + " not found!");
             else
                 Util.logDebug(LOGGER, "tag: " + element + " not found!");
-            return element;
+            return null;
         }
     }
 }
